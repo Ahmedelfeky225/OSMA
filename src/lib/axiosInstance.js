1af3b7ac6127358e77a5701dce6bf43adcb42848
Ahmed Elfeky <elfekyahmed337@gmail.com>
2025-08-06 18:42:50 +0300

    Fixes

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", // عدل حسب رابط API بتاعك
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`, // عدل حسب رابط API بتاعك
   headers: {
     "Content-Type": "application/json",
     AppId: process.env.NEXT_PUBLIC_APPID,
