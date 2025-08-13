5892714c4a5c4573a449601a58b73ce1cd7ef064
Ahmed Elfeky <elfekyahmed337@gmail.com>
2025-08-14 01:41:51 +0300

    Pushsw

--- src/utils/fetchInterceptor.js
+++ src/utils/fetchInterceptor.js
@@ -1,52 +1,16 @@
-// import { cookies } from "next/headers";
-// import { getLocale } from "next-intl/server";
-
-// export const fetchInterceptor = async (url, options = {}) => {
-//   const locale = await getLocale();
-//   const cookieStore = await cookies();
-//   const tokenCookie = cookieStore.get("token");
-
-//   let baseURL = `${process.env.NEXT_APP_BASE_URL}/${url}?locale=${locale}`;
-
-//   if (options?.params) {
-//     const params = new URLSearchParams(options.params).toString();
-//     baseURL = `${process.env.NEXT_APP_BASE_URL}/${url}?${params}&locale=${locale}`;
-//   }
-
-//   const modifiedOptions = {
-//     ...options,
-//     headers: {
-//       ...(options.headers || {}),
-//       "Content-Type": "application/json",
-//       appId: process.env.NEXT_PUBLIC_APPID,
-//       // Cookie: `token=${tokenCookie?.value || ""}`,
-//     },
-//     cache: "no-store",
-//   };
-
-//   try {
-//     const response = await fetch(baseURL, modifiedOptions);
-//     const data = await response.json();
-
-//     if (!response.ok) return null;
-//     return data;
-//   } catch (error) {
-//     console.error("❌ Fetch Interceptor Error:", error.message);
-//     return null;
-//   }
-// };
+import { cookies } from "next/headers";
+import { getLocale } from "next-intl/server";
 
 export const fetchInterceptor = async (url, options = {}) => {
-  const locale =
-    typeof window !== "undefined"
-      ? document.documentElement.lang || "en"
-      : "en";
+  const locale = await getLocale();
+  const cookieStore = await cookies();
+  const tokenCookie = cookieStore.get("token");
 
-  let baseURL = `${process.env.NEXT_PUBLIC_API_URL}/${url}?locale=${locale}`;
+  let baseURL = `${process.env.NEXT_APP_BASE_URL}/${url}?locale=${locale}`;
 
   if (options?.params) {
     const params = new URLSearchParams(options.params).toString();
-    baseURL = `${process.env.NEXT_PUBLIC_API_URL}/${url}?${params}&locale=${locale}`;
+    baseURL = `${process.env.NEXT_APP_BASE_URL}/${url}?${params}&locale=${locale}`;
   }
 
   const modifiedOptions = {
@@ -55,15 +19,17 @@ export const fetchInterceptor = async (url, options = {}) => {
       ...(options.headers || {}),
       "Content-Type": "application/json",
       appId: process.env.NEXT_PUBLIC_APPID,
+      // Cookie: `token=${tokenCookie?.value || ""}`,
     },
-    credentials: "include", // 👈 مهم جداً عشان الكوكي يتبعت
     cache: "no-store",
   };
 
   try {
     const response = await fetch(baseURL, modifiedOptions);
+    const data = await response.json();
+
     if (!response.ok) return null;
-    return await response.json();
+    return data;
   } catch (error) {
     console.error("❌ Fetch Interceptor Error:", error.message);
     return null;
