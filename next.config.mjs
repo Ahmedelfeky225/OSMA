bc28c8dbcd0a7efd4438a9e4647c4196edad3e06
Ahmed Elfeky <elfekyahmed337@gmail.com>
2025-08-19 02:54:54 +0300

    BUG FIXESSSSSSS

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,42 @@
+// import createNextIntlPlugin from "next-intl/plugin";
+// import withPWAInit from "next-pwa";
+
+// // إعداد next-intl
+// const withNextIntl = createNextIntlPlugin();
+
+// // إعداد PWA
+// const withPWA = withPWAInit({
+//   dest: "public", // مكان توليد service worker
+//   register: true, // يسجل تلقائيًا
+//   skipWaiting: true, // يفعّل أي تحديث فورًا
+//   disable: process.env.NODE_ENV === "development", // معطل أثناء التطوير
+//   buildExcludes: [/app-build-manifest\.json$/],
+// });
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     remotePatterns: [
+//       { protocol: "https", hostname: "res.cloudinary.com" },
+//       { protocol: "https", hostname: "cdn.salla.sa" },
+//     ],
+//   },
+//   // أي إعدادات إضافية هنا
+// };
+
+// // نلف PWA أولًا ثم Intl
+// export default withNextIntl(withPWA(nextConfig));
+
 import createNextIntlPlugin from "next-intl/plugin";
 import withPWAInit from "next-pwa";
 
-// إعداد next-intl
 const withNextIntl = createNextIntlPlugin();
-
-// إعداد PWA
 const withPWA = withPWAInit({
-  dest: "public", // مكان توليد service worker
-  register: true, // يسجل تلقائيًا
-  skipWaiting: true, // يفعّل أي تحديث فورًا
-  disable: process.env.NODE_ENV === "development", // معطل أثناء التطوير
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
   buildExcludes: [/app-build-manifest\.json$/],
 });
 
@@ -22,8 +49,8 @@ const nextConfig = {
       { protocol: "https", hostname: "cdn.salla.sa" },
     ],
   },
-  // أي إعدادات إضافية هنا
 };
 
-// نلف PWA أولًا ثم Intl
-export default withNextIntl(withPWA(nextConfig));
+const finalConfig = withNextIntl(nextConfig);
+
+export default withPWA(finalConfig);
