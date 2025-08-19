398201c47c63cfb598e62edf19b6e4e8f7a86eee
Ahmed Elfeky <elfekyahmed337@gmail.com>
2025-08-19 15:00:07 +0300

    FIXES HYPDREATE

--- src/components/layoutStructure.jsx
+++ src/components/layoutStructure.jsx
@@ -1,46 +1,66 @@
+// import { cookies } from "next/headers";
+// import { Navbar } from "@/components/navbar";
+// import Footer from "@/components/footer";
+// import { ReduxProvider } from "@/components/ReduxProvider";
+// import { Toaster } from "react-hot-toast";
+// import { fetchInterceptor } from "@/utils/fetchInterceptor";
+// import { Suspense } from "react";
+// import FixedWhatsappButton from "./fixedWhatsappButton";
+// import MuiProviders from "./MuiProviders";
+
+// export default async function LayoutStructure({ children }) {
+//   const cookieStore = await cookies();
+//   const cookieString = cookieStore.toString();
+
+//   const userData = await fetchInterceptor("auth/me", {
+//     headers: {
+//       Cookie: cookieString,
+//     },
+//   }).catch(() => null);
+
+//   // console.log("userData", userData);
+
+//   return (
+//     <ReduxProvider>
+//       <Navbar userData={userData} />
+
+//       <main>
+//         {/* <MuiProviders> */}
+//         {children}
+//         {/* </MuiProviders> */}
+//       </main>
+//       <Footer />
+//       <Toaster
+//         position="top-center"
+//         toastOptions={{
+//           className: "react-hot-toast",
+//           duration: 4000,
+//           style: { background: "#ffffff", color: "#131212" },
+//         }}
+//       />
+//       <Suspense fallback={null}>
+//         <FixedWhatsappButton />
+//       </Suspense>
+//     </ReduxProvider>
+//   );
+// }
+
+// LayoutStructureServer.js (Server Component)
 import { cookies } from "next/headers";
-import { Navbar } from "@/components/navbar";
-import Footer from "@/components/footer";
-import { ReduxProvider } from "@/components/ReduxProvider";
-import { Toaster } from "react-hot-toast";
 import { fetchInterceptor } from "@/utils/fetchInterceptor";
-import { Suspense } from "react";
-import FixedWhatsappButton from "./fixedWhatsappButton";
-import MuiProviders from "./MuiProviders";
+import ClientLayoutStructure from "./LayoutStructureClient";
 
 export default async function LayoutStructure({ children }) {
   const cookieStore = await cookies();
   const cookieString = cookieStore.toString();
 
   const userData = await fetchInterceptor("auth/me", {
-    headers: {
-      Cookie: cookieString,
-    },
+    headers: { Cookie: cookieString },
   }).catch(() => null);
 
-  // console.log("userData", userData);
-
   return (
-    <ReduxProvider>
-      <Navbar userData={userData} />
-
-      <main>
-        {/* <MuiProviders> */}
-        {children}
-        {/* </MuiProviders> */}
-      </main>
-      <Footer />
-      <Toaster
-        position="top-center"
-        toastOptions={{
-          className: "react-hot-toast",
-          duration: 4000,
-          style: { background: "#ffffff", color: "#131212" },
-        }}
-      />
-      <Suspense fallback={null}>
-        <FixedWhatsappButton />
-      </Suspense>
-    </ReduxProvider>
+    <ClientLayoutStructure userData={userData}>
+      {children}
+    </ClientLayoutStructure>
   );
 }
